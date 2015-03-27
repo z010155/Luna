6e140b675427595818284a84afb47280f00d8337
Romtoc <objcube@mail.com>
2015-03-27 14:34:24 +0530

    Small Changes

--- Utils/Cryptography.pm
+++ Utils/Cryptography.pm
@@ -11,33 +11,30 @@ method new {
        return $obj;
 }
 
-method encryptPass(Str $strPassword, Str $strKey) {
+method encryptPass($strPassword, $strKey) {
        my $strHash = $self->swapMD5(md5_hex($self->swapMD5($strPassword) . $strKey . 'Y(02.>\'H}t":E1'));
        return $strHash;
 }
 
-method swapMD5(Str $strHash) {
+method swapMD5($strHash) {
        my $strSwapped = substr($strHash, 16, 16);
        $strSwapped .= substr($strHash, 0, 16);
        return $strSwapped;
 }
 
-method reverseMD5(Str $strKey) {
+method reverseMD5($strKey) {
        my $revKey = reverse($strKey);
        my $strHash = md5_hex($revKey);
        return $strHash;
 }
 
 method generateKey {
-       my @arrChars = ('A'..'Z', 'a'..'z', 0..9, '!\"\A3$%^&*()_+-=[]{}:@~;#<>?|\\,./');
-       my $strKey = '';
-       for (0..9) {
-            $strKey .= $arrChars[rand(@arrChars)];
-       }
+       my @chars = ('A'..'Z', 'a'..'z', 0..9, '!$%^&*()_+-=[]{}:@~;<>?|\,./');
+       my $strKey = join '', map { @chars[rand @chars] } 1..8;
        return $strKey;
 }
 
-method generateInt(Int $intMin, Int $intMax) {
+method generateInt($intMin, $intMax) {
        my $intRand = rand($intMax - $intMin);
        my $intFinal = int($intMin + $intRand);
        return $intFinal;
