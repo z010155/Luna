da705b98b4172fef794aaffa1a8a20daa8a47632
Blake <tientavu@hotmail.com>
2015-03-30 16:27:32 -0500

    Temporary fix to get ninja progress popup working

--- Server/Systems/Ninja.pm
+++ Server/Systems/Ninja.pm
@@ -11,16 +11,29 @@ method new($resChild) {
        return $obj;
 }
 
-method handleGetNinjaRanks($strData, $objClient) {}
+method handleGetNinjaRanks($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       $objClient->sendXT(['gnr', '-1', $arrData[5], '1', '2', '3', '4']); #arrData[5] being player id, later?
+}
 
-method handleGetNinjaLevel($strData, $objClient) {}
+method handleGetNinjaLevel($strData, $objClient) {
+       $objClient->sendXT(['gnl', '-1', '0', '0']); # Level (BELT) // % To Next Level
+}
 
-method handleGetCards($strData, $objClient) {}
+method handleGetCards($strData, $objClient) {
+       $objClient->sendXT(['gcd', '-1']); # %xt%gcd%-1%101,1|102,1|card_id,card_quantity% ; later...
+}
 
-method handleGetFireLevel($strData, $objClient) {}
+method handleGetFireLevel($strData, $objClient) {
+       $objClient->sendXT(['gfl', '-1', '0', '0']); # Level (BELT) // % To Next Level
+}
 
-method handleGetWaterLevel($strData, $objClient) {}
+method handleGetWaterLevel($strData, $objClient) {
+       $objClient->sendXT(['gwl', '-1', '0', '0']); # Level (BELT) // % To Next Level
+}
 
-method handleGetSnowLevel($strData, $objClient) {}
+method handleGetSnowLevel($strData, $objClient) {
+       $objClient->sendXT(['gsl', '-1', '0', '0']); # Level (BELT) // % To Next Level
+}
 
 1;
