5921bd21a232805df705e0d2f0b9ac8b3b862f80
Blake <tientavu@hotmail.com>
2015-03-30 15:58:28 -0500

    HUGE PUFFLE ADDITIONS!
    EVERYTHING seems to be working.
    
    Removed phg handler because it never existed (wtf iCPPS?)

--- Server/Systems/Pets.pm
+++ Server/Systems/Pets.pm
@@ -34,50 +34,131 @@ method handleGetPuffle($strData, $objClient) {
        $objClient->sendXT(['pg', '-1', $objClient->getPuffles($puffleID)]);
 }
 
-method handlePuffleBath($strData, $objClient) {}
-
-method handlePuffleFeed($strData, $objClient) {}
-
-method handlePuffleRest($strData, $objClient) {}
-
-method handlePuffleIsResting($strData, $objClient) {}
-
-method handlePufflePlay($strData, $objClient) {}
-
-method handlePuffleFeedFood($strData, $objClient) {}
-
-method handlePuffleIsPlaying($strData, $objClient) {}
-
-method handlePuffleMove($strData, $objClient) {
+method handlePuffleBath($strData, $objClient) { 
        my @arrData = split('%', $strData);
-       $objClient->sendXT(['pm', '-1', $arrData[5], $arrData[6], $arrData[7]]);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       if ($objClient->{coins} < 5) {
+           return $objClient->sendError(401);
+       }
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->setCoins($objClient->{coins} - 5);
+       $objClient->sendRoom('%xt%pb%-1%'.$objClient->{coins}.'%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%');
 }
 
-method handlePuffleClick($strData, $objClient) {
-       $objClient->sendXT(['phg', '1836', '1']);
+method handlePuffleFeed($strData, $objClient) { 
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       my $puffleTreat = $arrData[6];
+       return if (!int($puffleID) || !int($puffleTreat) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       if ($objClient->{coins} < 5) {
+           return $objClient->sendError(401);
+       }
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->setCoins($objClient->{coins} - 5);
+       $objClient->sendRoom('%xt%pt%-1%'.$objClient->{coins}.'%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%'.$puffleTreat.'%');
 }
 
-method handlePuffleUser($strData, $objClient) {}           
+method handlePuffleRest($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->sendRoom('%xt%pr%-1%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%');
+}
 
 method handlePufflePip($strData, $objClient) {
        my @arrData = split('%', $strData);
-       $objClient->sendXT(['pip', '-1', $arrData[5], $arrData[6], $arrData[7]]);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->sendRoom('%xt%pir%-1%'.$petDetails->{puffleID}.'%'.$arrData[6].'%'.$arrData[7].'%');
+}
+
+method handlePufflePlay($strData, $objClient) { 
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       # OK, Play Types - 0:normal, 1:super, 2:great
+       $objClient->sendRoom('%xt%pp%-1%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%'.int(rand(2)).'%');
+}
+
+method handlePuffleFeedFood($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       if ($objClient->{coins} < 10) {
+           return $objClient->sendError(401);
+       }
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->setCoins($objClient->{coins} - 10);
+       $objClient->sendRoom('%xt%pf%-1%'.$objClient->{coins}.'%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%');
 }
 
 method handlePufflePir($strData, $objClient) {
        my @arrData = split('%', $strData);
-       $objClient->sendXT(['pir', '-1', $arrData[5], $arrData[6], $arrData[7]]);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->sendRoom('%xt%pir%-1%'.$petDetails->{puffleID}.'%'.$arrData[6].'%'.$arrData[7].'%');
+}
+
+method handlePuffleMove($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       $objClient->sendRoom('%xt%pm%-1%'.$arrData[5].'%'.$arrData[6].'%'.$arrData[7].'%');
+}
+
+method handlePuffleUser($strData, $objClient) {
+       $objClient->sendXT(['pgu', '-1', $objClient->getPuffles($objClient->{ID})]);
+}           
+
+method handlePuffleIsPlaying($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->sendRoom('%xt%ip%-1%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%'.$arrData[6].'%'.$arrData[7].'%');
+
+}
+
+method handlePuffleIsFeeding($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->sendRoom('%xt%if%-1%'.$objClient->{coins}.'%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%'.$arrData[6].'%'.$arrData[7].'%');
+
+}
+
+method handlePuffleIsResting($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       my $puffleID = $arrData[5];
+       return if (!int($puffleID) || !int($arrData[6]) || !int($arrData[7]) || $self->{child}->{modules}->{mysql}->countRows("SELECT `ownerID` FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'") eq 0);
+       my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
+       $objClient->sendRoom('%xt%ir%-1%'.$petDetails->{puffleID}.'|'.$petDetails->{puffleName}.'|'.$petDetails->{puffleType}.'|100|100|100%'.$arrData[6].'%'.$arrData[7].'%');
+}
+
+method handleSendPuffleFrame($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       # Not sure if it's valid... will check later
+       $objClient->sendRoom('%xt%ps%-1%'.$arrData[5].'%'.$arrData[6].'%');
 }
 
 method handlePuffleWalk($strData, $objClient) {
        my @arrData = split('%', $strData);
        my $puffleID = $arrData[5];
-       return if (!int($puffleID));
+       return if (!int($puffleID) || !int($arrData[6]));
        my $petDetails = $self->{child}->{modules}->{mysql}->fetchColumns("SELECT * FROM puffles WHERE `puffleID` = '$puffleID' AND `ownerID` = '$objClient->{ID}'");
        if ($petDetails) {
-           $objClient->updatePlayerCard('upa', 'hand', 75 . $petDetails->{puffleType});
-           my $walkStr = $petDetails->{puffleID} . '|' . $petDetails->{puffleName} . '|' . $petDetails->{puffleType} . '|0|0|0|0|0|1';
-           $objClient->sendRoom('%xt%pw%-1%'. $objClient->{ID} . '%' . $walkStr . '%');
+                my $walkStr = $petDetails->{puffleID} . '|' . $petDetails->{puffleName} . '|' . $petDetails->{puffleType} . '|100|100|100|0|0|0|0|0|0';
+           if ($arrData[6] eq 1) {
+                $objClient->updatePlayerCard('upa', 'hand', 75 . $petDetails->{puffleType});
+                $objClient->sendRoom('%xt%pw%-1%'. $objClient->{ID} . '%' . $walkStr . '|1%');
+           } else {
+                $objClient->updatePlayerCard('upa', 'hand', 0);
+                $objClient->sendRoom('%xt%pw%-1%'. $objClient->{ID} . '%' . $walkStr . '|0%');
+           }
        }
 }
 
