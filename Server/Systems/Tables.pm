58051a0c20c9c4eb07c1554ec456dcba4f4f1614
Blake <tientavu@hotmail.com>
2015-03-30 16:48:01 -0500

    Update Tables.pm

--- Server/Systems/Tables.pm
+++ Server/Systems/Tables.pm
@@ -11,12 +11,23 @@ method new($resChild) {
        return $obj;
 }
 
-method handleJoinTable($strData, $objClient) {}
+method handleJoinTable($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       return if(!int($arrData[5]));
+       $objClient->sendXT(['jt', '-1']); #TableID%SeatID
+}
 
-method handleGetTable($strData, $objClient) {}
+method handleGetTable($strData, $objClient) {
+       $objClient->sendXT(['gt', '-1']);
+}
 
-method handleUpdateTable($strData, $objClient) {}
+method handleUpdateTable($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       $objClient->sendRoom('%xt%ut%'.$arrData[5].'%'.$arrData[6].'%'); # TableID%Players
+}
 
-method handleLeaveTable($strData, $objClient) {}
+method handleLeaveTable($strData, $objClient) {
+       # really there's nothing to send
+}
 
 1;
