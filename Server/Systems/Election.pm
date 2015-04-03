6a7951dbf782a923c3bd390ea431fbbacdd61c0e
Blake <tientavu@hotmail.com>
2015-04-03 15:07:09 -0500

    Donate coins added
    Still finding handlers for the handleSetPoll(), but donate coins should work now!

--- Server/Systems/Election.pm
+++ Server/Systems/Election.pm
@@ -11,8 +11,20 @@ method new($resChild) {
        return $obj;
 }
 
-method handleDonateCoins($strData, $objClient) {}
+method handleDonateCoins($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       my $id = $arrData[5]; # Poll ID? dunno.
+       my $ammount = $arrData[6]; # Amount of coins to donate
+       if ($objClient->{coins} < $ammount || !int($ammount)) {
+              return $objClient->sendError(401);
+       }
+       $objClient->setCoins($objClient->{coins} - $ammount);
+       $objClient->sendXT(['dc', $id, $objClient->{coins}]);
+}
 
-method handleSetPoll($strData, $objClient) {}
+method handleSetPoll($strData, $objClient) {
+       my @arrData = split('%', $strData);
+       # Don't have handlers right now
+}
 
 1;
